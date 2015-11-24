27102aac334807b6d904673d2e04fcb441826ab0
RyanPPS <ryan@paragonpoolspa.com>
2015-11-24 15:34:04 -0800

    updating

---
+++ server/publications/products.js
@@ -0,0 +1,45 @@
+/**
+ * products publication
+ * @param {Number} productScrollLimit - optional, defaults to 20
+ * @param {Array} shops - array of shopId to retrieve product from.
+ * @return {Object} return product cursor
+ */
+Meteor.publish("ProductImport", function (shops) {
+  check(shops, Match.Optional(Array));
+
+  let shopAdmin;
+  let shop = ReactionCore.getCurrentShop(this);
+  let Products = ReactionCore.Collections.Products;
+  if (shop) {
+    let selector = {
+      shopId: shop._id
+    };
+    // handle multiple shops
+    if (shops) {
+      selector = {
+        shopId: {
+          $in: shops
+        }
+      };
+      // check if this user is a shopAdmin
+      for (let thisShop of shops) {
+        if (Roles.userIsInRole(this.userId, ["admin", "createProduct"],
+            thisShop._id)) {
+          shopAdmin = true;
+        }
+      }
+    }
+
+    // products are always visible to owners
+    if (!(Roles.userIsInRole(this.userId, ["owner"], shop._id) || shopAdmin)) {
+      selector.isVisible = true;
+    }
+
+    return Products.find(selector, {
+      sort: {
+        title: 1
+      }
+    });
+  }
+  this.ready();
+});
